5f939b56c61689798f26c14c5c325f5f32ae16f8
Olav Kihle <olavki@stud.ntnu.no>
2023-09-17 16:56:35 +0200

    Show picture of preview

--- screens/Camera.tsx
+++ screens/Camera.tsx
@@ -1,32 +1,18 @@
 import React, { useRef, useState } from 'react';
-import {
-  Button,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import { Camera, CameraType } from 'expo-camera';
+import { Button, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import { Camera, CameraCapturedPicture, CameraType } from 'expo-camera';
 import { useNavigation } from '@react-navigation/native';
 import { analyze } from '../cloud/analyze';
-
-const takePicture = (cameraRef: React.RefObject<Camera>) => async () => {
-  if (!cameraRef.current) {
-    return;
-  }
-
-  const options = { quality: 0.5, base64: true };
-  const data = await cameraRef.current?.takePictureAsync(options);
-
-  return data?.base64;
-};
+import CameraPreview from './CameraPreview';
+import { styles } from './styles';
 
 const CameraScreen = () => {
   const navigation = useNavigation();
   const cameraRef = useRef(null);
   const [type, setType] = useState(CameraType.back);
   const [prediction, setPrediction] = useState<string | 'Loading'>();
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [image, setImage] = useState<CameraCapturedPicture | null>(null);
 
   const [permission, requestPermission] = Camera.useCameraPermissions();
 
@@ -47,76 +33,84 @@ const CameraScreen = () => {
     );
   }
 
+  const takePicture = (cameraRef: React.RefObject<Camera>) => async () => {
+    if (!cameraRef.current) {
+      return;
+    }
+
+    const options = { quality: 0.5, base64: true };
+    const data = await cameraRef.current?.takePictureAsync(options);
+    console.log(data);
+    setPreviewVisible(true);
+    setImage(data);
+    return data?.base64;
+  };
+
+  const retakePicture = () => {
+    setPreviewVisible(false);
+    setImage(null);
+  };
+
+  const savePicture = () => {
+    setPreviewVisible(false);
+  };
+
   return (
     <View style={styles.flex}>
-      <Camera style={styles.camera} type={type} ref={cameraRef}>
-        <SafeAreaView style={styles.container}>
-          <Text style={styles.prediction}>{prediction ?? ''}</Text>
-          <View style={styles.buttonContainer}>
+      {previewVisible && image ? (
+        CameraPreview({
+          photo: image,
+          retakePhoto: retakePicture,
+          savePhoto: savePicture,
+        })
+      ) : (
+        <Camera style={styles.camera} type={type} ref={cameraRef}>
+          <SafeAreaView style={styles.buttonContainer}>
+            <Text style={styles.prediction}>{prediction ?? ''}</Text>
             <TouchableOpacity
               style={styles.button}
               onPress={() => navigation.navigate('History' as never)}
             >
               <Text style={styles.text}>History</Text>
             </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={async () => {
-                setPrediction('Loading');
-                const pictureData = await takePicture(cameraRef)();
-                if (pictureData) await analyzePicture(pictureData);
-              }}
-            >
-              <Text style={styles.text}>Take Picture</Text>
+            =
+            <TouchableOpacity style={styles.button} onPress={takePicture(cameraRef)}>
+              <View
+                style={{
+                  position: 'absolute',
+                  bottom: 0,
+                  flexDirection: 'row',
+                  flex: 1,
+                  width: '100%',
+                  padding: 20,
+                  justifyContent: 'space-between',
+                }}
+              >
+                <View
+                  style={{
+                    alignSelf: 'center',
+                    flex: 1,
+                    alignItems: 'center',
+                  }}
+                >
+                  <TouchableOpacity
+                    onPress={takePicture(cameraRef)}
+                    style={{
+                      width: 70,
+                      height: 70,
+                      bottom: 0,
+                      borderRadius: 50,
+                      backgroundColor: '#fff',
+                    }}
+                  />
+                </View>
+              </View>
             </TouchableOpacity>
-          </View>
-        </SafeAreaView>
-      </Camera>
+          </SafeAreaView>
+        </Camera>
+      )}
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  permissionContainer: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  flex: {
-    flex: 1,
-  },
-  camera: {
-    flex: 1,
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  prediction: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    backgroundColor: 'black',
-    padding: 20,
-    color: 'white',
-    textAlign: 'center',
-  },
-});
 
 export default CameraScreen;
---
+++ screens/styles.ts
@@ -0,0 +1,51 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  permissionContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  flex: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  darkText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  prediction: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    backgroundColor: 'black',
+    padding: 20,
+    color: 'white',
+    textAlign: 'center',
+  },
+});
