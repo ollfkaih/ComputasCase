a095cc03816196d65a09472bff270224150a4769
Olav Kihle <olavki@stud.ntnu.no>
2023-09-17 14:12:02 +0200

    Basic camera view

--- App.tsx
+++ App.tsx
@@ -1,20 +1,79 @@
-import { SafeAreaView, StyleSheet } from 'react-native';
-import Navigation from './navigation';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Camera, CameraType } from 'expo-camera';
 
-export default function App() {
+const takePicture = (cameraRef: React.RefObject<Camera>) => async () => {
+  if (!cameraRef.current) {
+    return;
+  }
+
+  const options = { quality: 0.5, base64: true };
+  const data = await cameraRef.current?.takePictureAsync(options);
+  console.log(data.base64);
+};
+
+const App = () => {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const cameraRef = useRef(null);
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Navigation />
-      {/*<StatusBar style="auto" />*/}
-    </SafeAreaView>
+    <View style={styles.flex}>
+      <Camera style={styles.camera} type={type} ref={cameraRef}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={takePicture(cameraRef)}>
+            <Text style={styles.text}>Take Picture</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  flex: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
+
+export default App;
